fa0559b8a5deaa190a7de11d1e3fb68dc2b22c95
Ujjawal kaushik <221929656+ujjawalkaushik1110@users.noreply.github.com>
2026-09-24 09:59:04 +0530

    Add Victus 8DD0 regression coverage and watchdog failsafe

---
+++ src/OmenCoreApp.Tests/Hardware/Victus8DD0CapabilityTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using OmenCore.Hardware;
+using Xunit;
+
+namespace OmenCoreApp.Tests.Hardware
+{
+    public class Victus8DD0CapabilityTests
+    {
+        [Fact]
+        public void GetCapabilities_ReturnsVerifiedProfile_ForVictus8DD0()
+        {
+            var capabilities = ModelCapabilityDatabase.GetCapabilities("8DD0");
+
+            capabilities.Should().NotBeNull();
+            capabilities.ProductId.Should().Be("8DD0");
+            capabilities.ModelName.Should().Be("HP Victus 15 (2025) fb3xxx");
+
+            capabilities.UserVerified.Should().BeFalse();
+            capabilities.SupportsFanControlWmi.Should().BeTrue();
+            capabilities.SupportsFanControlEc.Should().BeFalse();
+            capabilities.SupportsFanCurves.Should().BeTrue();
+            capabilities.SupportsIndependentFanCurves.Should().BeFalse();
+            capabilities.FanZoneCount.Should().Be(1);
+            capabilities.AllowDecoupledWmiThermalPolicyFallback.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetPreferredCapabilities_PrefersExact8DD0Profile_OverFb3PatternFallback()
+        {
+            var capabilities = ModelCapabilityDatabase.GetPreferredCapabilities(
+                "8DD0",
+                "Victus by HP Gaming Laptop 15-fb3xxx",
+                CpuUndervoltProviderFactory.CpuVendor.AMD);
+
+            capabilities.Should().NotBeNull();
+            capabilities!.ProductId.Should().Be("8DD0");
+            capabilities.UserVerified.Should().BeFalse();
+            capabilities.SupportsFanControlWmi.Should().BeTrue();
+        }
+    }
+}
---
+++ src/OmenCoreApp.Tests/Services/HardwareWatchdogServiceTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using FluentAssertions;
+using OmenCore.Services;
+using OmenCore.Services.Diagnostics;
+using Xunit;
+
+namespace OmenCoreApp.Tests.Services
+{
+    public class HardwareWatchdogServiceTests
+    {
+        private static HardwareWatchdogService CreateService()
+        {
+            var logging = new LoggingService();
+            logging.Initialize();
+
+            // The watchdog tests only exercise watchdog state. A completely uninitialized
+            // FanService is sufficient because any attempted hardware call is caught by
+            // HardwareWatchdogService.
+            var fanService = (FanService)RuntimeHelpers.GetUninitializedObject(typeof(FanService));
+
+            return new HardwareWatchdogService(
+                logging,
+                fanService,
+                new ResumeRecoveryDiagnosticsService());
+        }
+
+        private static void SetField<T>(object target, string name, T value)
+        {
+            var field = target.GetType().GetField(
+                name,
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+            field.Should().NotBeNull($"private watchdog field '{name}' must exist");
+            field!.SetValue(target, value);
+        }
+
+        private static T GetField<T>(object target, string name)
+        {
+            var field = target.GetType().GetField(
+                name,
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+            field.Should().NotBeNull($"private watchdog field '{name}' must exist");
+            return (T)field!.GetValue(target)!;
+        }
+
+        [Fact]
+        public void Failsafe_DoesNotRelease_WhileTemperatureIsAboveSafeThreshold()
+        {
+            var sut = CreateService();
+
+            SetField(sut, "_failsafeActive", true);
+            SetField(sut, "_isWatchdogArmed", false);
+            SetField(sut, "_failsafeSafeSinceUtc", DateTime.UtcNow.AddSeconds(-60));
+
+            sut.UpdateTemperature(70, 60);
+
+            GetField<bool>(sut, "_failsafeActive").Should().BeTrue();
+            GetField<bool>(sut, "_isWatchdogArmed").Should().BeFalse();
+            GetField<DateTime>(sut, "_failsafeSafeSinceUtc")
+                .Should().Be(DateTime.MinValue);
+        }
+
+        [Fact]
+        public void Failsafe_DoesNotRelease_WhenTemperaturesAreInvalid()
+        {
+            var sut = CreateService();
+
+            SetField(sut, "_failsafeActive", true);
+            SetField(sut, "_isWatchdogArmed", false);
+            SetField(sut, "_failsafeSafeSinceUtc", DateTime.UtcNow.AddSeconds(-60));
+
+            sut.UpdateTemperature(0, 0);
+
+            GetField<bool>(sut, "_failsafeActive").Should().BeTrue();
+            GetField<bool>(sut, "_isWatchdogArmed").Should().BeFalse();
+            GetField<DateTime>(sut, "_failsafeSafeSinceUtc")
+                .Should().Be(DateTime.MinValue);
+        }
+
+        [Fact]
+        public void Failsafe_Releases_AfterSustainedSafeTemperatures()
+        {
+            var sut = CreateService();
+
+            SetField(sut, "_failsafeActive", true);
+            SetField(sut, "_isWatchdogArmed", false);
+            SetField(
+                sut,
+                "_failsafeSafeSinceUtc",
+                DateTime.UtcNow.AddSeconds(-16));
+
+            sut.UpdateTemperature(60, 55);
+
+            GetField<bool>(sut, "_failsafeActive").Should().BeFalse();
+            GetField<bool>(sut, "_isWatchdogArmed").Should().BeTrue();
+        }
+    }
+}
